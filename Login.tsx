081dcc07bb895380a5d31c60f20db25cb715a63c
IrmandoKoyo <irmandokoyo@protonmail.com>
2025-09-22 11:16:31 +0800

    Exercise#6

--- Login.tsx
+++ Login.tsx
@@ -1,38 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Title from './components/Title';
 import TextInput from './components/TextInput';
 import Button from './components/Button';
 
-const Login = () => {
-  const onSignIn = () => {
-    console.log('Button di klik');
-  };
+const Register = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
 
+  const onRegister = () => {
+    console.log('Button di klik');
+    console.log('Name:', name);
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Address:', address);
+    console.log('Phone:', phone);
+  };
 
   return (
     <View style={styles.container}>
-      <Title>Welcome</Title>
+      <Title>Registration</Title> 
 
-      <TextInput placeholder="Masukan username anda" label="Username" />
-      <TextInput
-        placeholder="Masukan password anda"
-        label="Password"
-        secureTextEntry={true}
+      <TextInput 
+        placeholder="Masukan nama lengkap anda" 
+        label="Name"
+        value={name}
+        onChangeText={setName}
       />
-      <Button label="Sign in" onPress={onSignIn} />
+      <TextInput 
+        placeholder="Masukan username anda" 
+        label="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput 
+        placeholder="Masukan email anda" 
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput 
+        placeholder="Masukan alamat anda" 
+        label="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <TextInput 
+        placeholder="Masukan Nomor telepon anda" 
+        label="Phone Number"
+        value={phone}
+        onChangeText={setPhone}
+      />
 
-      <Button label="Create New Account" color="#797171" colorText="#ffffff" />
-
-      <Button label="Sign in with Google" color="#DB4437" colorText="#ffffff" />
-      <Button label="Sign in with Facebook" color="#1877F2" colorText="#ffffff" />
-      <Button label="Sign in with Apple" color="#000000" colorText="#ffffff" />
-      
+      <Button label="Register" onPress={onRegister} />
     </View>
   );
 };
 
-export default Login;
+export default Register;
 
 const styles = StyleSheet.create({
   container: {
