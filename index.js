afec6c86bda56b43a94a7d627c0bcc1f800c9341
IrmandoKoyo <irmandokoyo@protonmail.com>
2025-09-29 12:12:17 +0800

    Exercise#7

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Flexbox from './Flexbox';
 import Login from './Login';
 
-AppRegistry.registerComponent(appName, () => Login);
+
+
+AppRegistry.registerComponent(appName, () => Flexbox);
